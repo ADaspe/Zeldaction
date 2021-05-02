aed5720b2e8cc9d8bd98785f190dd2635b33bf1c
Alexandre Daspe <alexandre.daspe@gmail.com>
2021-05-02 19:35:07 +0200

    MushroomSpawner OK

--- Assets/Scripts/AXD_Scripts/AXD_MushroomSpawner.cs
+++ Assets/Scripts/AXD_Scripts/AXD_MushroomSpawner.cs
@@ -5,5 +5,20 @@ using UnityEngine;
 public class AXD_MushroomSpawner : MonoBehaviour
 {
     public bool activated;
-    
+    public AXD_Mushroom mushroomPrefab;
+    public AXD_Mushroom ActiveMushroom;
+    public float MushroomNextrSpawn;
+    public float secondsToAddToExplodingTimeForSpawning;
+
+    private void FixedUpdate()
+    {
+        if (activated)
+        {
+            if(ActiveMushroom == null && MushroomNextrSpawn >= Time.time)
+            {
+                MushroomNextrSpawn = mushroomPrefab.ExplodingTime + secondsToAddToExplodingTimeForSpawning;
+                ActiveMushroom = Instantiate(mushroomPrefab, transform);
+            }
+        }
+    }
 }
