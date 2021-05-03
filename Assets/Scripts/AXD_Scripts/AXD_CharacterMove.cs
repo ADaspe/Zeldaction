b616ab5cc4bbdab1eaa4dfa7d25ba1a8aee78fd2
Eliott <eliott.chauchard@orange.fr>
2021-05-03 17:06:46 +0200

    Animations + autoaim amélioré

--- Assets/Scripts/AXD_Scripts/AXD_CharacterMove.cs
+++ Assets/Scripts/AXD_Scripts/AXD_CharacterMove.cs
@@ -73,11 +73,11 @@ public class AXD_CharacterMove : MonoBehaviour
     {
         if(rb.velocity.magnitude >= 0.005f)
         {
-            AnimManager.UpdateAnimations(charaManager.PlayerWalk);
+            AnimManager.isMoving = true;
         }
         else
         {
-            AnimManager.UpdateAnimations(charaManager.PlayerIdle);
+            AnimManager.isMoving = false;
         }
     }
 
