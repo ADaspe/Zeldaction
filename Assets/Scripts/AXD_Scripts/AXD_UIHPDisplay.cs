4e44ab983bbf2a05eee4cb8efaae83fbbaf5b65f
Alexandre Daspe <alexandre.daspe@gmail.com>
2021-05-20 17:23:06 +0200

    UI Life lost on hit

--- Assets/Scripts/AXD_Scripts/AXD_UIHPDisplay.cs
+++ Assets/Scripts/AXD_Scripts/AXD_UIHPDisplay.cs
@@ -8,35 +8,45 @@ public class AXD_UIHPDisplay : MonoBehaviour
 {
     public ELC_CharacterManager charaManager;
     public GameObject HPPrefab;
-    public List<AXD_HP> allHPs;
+    public AXD_HP[] allHPs ;
 
     private void Start()
     {
+        allHPs = new AXD_HP[6];
         for (int i = 0; i < charaManager.currentHP; i++)
         {
-            allHPs.Add(Instantiate(HPPrefab, this.transform).GetComponent<AXD_HP>());
+            allHPs[i] = (Instantiate(HPPrefab, this.transform).GetComponent<AXD_HP>());
 
         }
     }
     public void LoseLife()
     {
-
+        allHPs[charaManager.currentHP - 1].EmptyHP();
     }
 
     public void HealLife()
     {
-
+        allHPs[charaManager.currentHP - 1].FullfillHP();
     }
 
     public void HealFullLife()
     {
-        
+        for (int i = 0; i < allHPs.Length; i++)
+        {
+            if (allHPs[i] != null)
+            {
+                allHPs[i].FullfillHP();
+            }
+        }
     }
 
     [Button]
     public void AddLife()
     {
-        allHPs.Add(Instantiate(HPPrefab, this.transform).GetComponent<AXD_HP>());
+        if (charaManager.maxHP < 6)
+        {
+            allHPs[charaManager.maxHP] = (Instantiate(HPPrefab, this.transform).GetComponent<AXD_HP>());
+        }
     }
 
 }
