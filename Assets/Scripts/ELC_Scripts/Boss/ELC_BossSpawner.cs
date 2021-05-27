a1f9573abc703903d22faebd77a1c1fdc5f91bc4
Eliott <eliott.chauchard@gmail.com>
2021-05-27 16:39:59 +0200

    Intro Boss + corrections torches

--- Assets/Scripts/ELC_Scripts/Boss/ELC_BossSpawner.cs
+++ Assets/Scripts/ELC_Scripts/Boss/ELC_BossSpawner.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class ELC_BossSpawner : MonoBehaviour
 {
     public GameObject BossGO;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Ryn")) BossGO.SetActive(true);
+        BossGO.GetComponent<ELC_BossManager>().SwitchPhase();
+        this.GetComponent<Collider2D>().enabled = false;
+        
     }
 }
