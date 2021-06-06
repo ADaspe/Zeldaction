81bd61ab5b6110fb3f666f88d8e42032b8b35bf1
Eliott <eliott.chauchard@gmail.com>
2021-06-06 15:54:07 +0200

    Dissolve ronces

--- Assets/Scripts/AXD_Scripts/AXD_Brambles.cs
+++ Assets/Scripts/AXD_Scripts/AXD_Brambles.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class AXD_Brambles : MonoBehaviour
 {
+    public Animator anims;
     public void Purify()
+    {
+        anims.SetBool("Dissolve", true);
+        Invoke("Destroy", 1f);
+    }
+
+    private void Destroy()
     {
         Destroy(this.gameObject);
     }
